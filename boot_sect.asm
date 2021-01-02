88c4e648651c2fe37d4010d18b1a04742d4b842a
İsmail Tüzün <evliyali.tzn@gmail.com>
2021-01-02 16:35:02 +0300

    Printed hello to screen using BIOS routine interrupts

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,9 +1,23 @@
 ;
-; A simple boot sector program that loops forever.
+; A simple boot sector that prints a message to the screen
+; using a BIOS routine.
 ;
 
-loop :
-  jmp loop
+
+mov ah, 0x0e
+
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+
+jmp $
 
 times 510 - ($ - $$) db 0
 
