2ad1c58c4cc197ba42a45901a6489eec65afaea5
İsmail Tüzün <evliyali.tzn@gmail.com>
2021-01-02 18:42:26 +0300

    Added simple boot sector to demonstrate find the byte exercise

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,23 +1,43 @@
 ;
-; A simple boot sector that prints a message to the screen
-; using a BIOS routine.
+; A simple boot sector that demonstrate "find the byte" exercise.
 ;
 
+[org 0x7c00]
 
 mov ah, 0x0e
 
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'o'
+mov al, "1"
 int 0x10
 
+mov al, the_secret
+int 0x10
+
+mov al, "2"
+int 0x10
+
+mov al, [the_secret]
+int 0x10
+
+mov al, "3"
+int 0x10
+
+mov bx, the_secret
+add bx, 0x7c00
+mov al, [bx]
+int 0x10
+
+mov al, "4"
+int 0x10
+
+mov al, [0x7c2d]
+int 0x10
+
+
 jmp $
+
+the_secret:
+  db "X"
+
 
 times 510 - ($ - $$) db 0
 
